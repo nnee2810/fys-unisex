86b98ecb0cf4d7d8a1a3b4bfa6b592048800991d
Namee2810 <dvn281002@gmail.com>
2022-07-09 22:28:36 +0700

    fix: flipmove types

---
+++ src/components/NextFlipMove.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react"
+import FlipMove from "react-flip-move"
+import styled from "styled-components"
+
+interface NextFlipMoveProps {
+  children: ReactNode
+}
+
+export const NextFlipMove = styled(FlipMove)<NextFlipMoveProps>``
--- src/modules/users/components/address/AddressList.tsx
+++ src/modules/users/components/address/AddressList.tsx
@@ -5,9 +5,8 @@ import {
   Box,
   Divider,
 } from "@chakra-ui/react"
-import { StackSkeleton } from "components"
+import { NextFlipMove, StackSkeleton } from "components"
 import { useGetAddressList } from "modules/users/hooks"
-import FlipMove from "react-flip-move"
 import { AddressBox } from "./AddressBox"
 
 export function AddressList() {
@@ -19,14 +18,14 @@ export function AddressList() {
       {isLoadingGetAddressList ? (
         <StackSkeleton value={5} h="100px" />
       ) : dataGetAddressList?.length ? (
-        <FlipMove enterAnimation="fade" leaveAnimation="fade">
+        <NextFlipMove enterAnimation="fade" leaveAnimation="fade">
           {dataGetAddressList.map((address, idx) => (
             <Box key={address.id}>
               <AddressBox data={address} />
               {idx < dataGetAddressList.length - 1 && <Divider my="5" />}
             </Box>
           ))}
-        </FlipMove>
+        </NextFlipMove>
       ) : (
         <Alert status="warning" borderRadius="6" variant="left-accent">
           <AlertIcon />
